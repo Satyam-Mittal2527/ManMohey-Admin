394dbad894dffbaee8525180e7d8f33d4ac68e8e
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2026-09-12 15:12:43 +0530

    BackendCorsFix

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,11 +28,21 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://man-mohey-admin.vercel.app",
     ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=[
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+        "OPTIONS",
+    ],
+    allow_headers=[
+        "Authorization",
+        "Content-Type",
+    ],
 )
 
 
